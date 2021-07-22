38aa9c19874418bb255430eb3d1cd146f78a1627
LienPaul <zinfu1111@gmail.com>
2021-07-23 02:11:49 +0800

    Initial Commit

---
+++ MegaEBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MegaEBook
+//
+//  Created by 連振甫 on 2021/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
